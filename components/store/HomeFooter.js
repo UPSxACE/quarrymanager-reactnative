b77278712c1eaef9928c313fe2fce269581a729f
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-10-23 17:21:06 +0100

    componente HomeFooter #4

---
+++ components/store/HomeFooter.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function HomeFooter() {
+  return (
+    <View>
+      <Text>
+        Não tens uma conta? <Text>Regista-te</Text>.
+      </Text>
+    </View>
+  );
+}
