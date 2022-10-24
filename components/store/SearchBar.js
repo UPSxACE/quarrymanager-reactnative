153d78ac1496066ea8ab58a5794c366b36338b1a
Joao Rodrigo Goncalves Carvalho <a44737@alunos.ipb.pt>
2022-10-24 21:46:06 +0100

    teste#9

--- components/store/SearchBar.js
+++ components/store/SearchBar.js
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
 export default function SearchBar(props) {
   return (
     <View>
-      <Text>Search Bar</Text>
+      <Text>Search Bar test</Text>
     </View>
   );
 }
