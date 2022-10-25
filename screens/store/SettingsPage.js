c48e1ba10c27d53f2c39f5c1372e7c34a3115c09
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-10-25 12:57:24 +0100

    paddings do arrowButton #11

--- screens/store/SettingsPage.js
+++ screens/store/SettingsPage.js
@@ -1,25 +1,28 @@
 import * as React from "react";
 import ArrowButton from "../../components/store/ArrowButton";
+import NavBar from "../../components/store/NavBar";
 import { Center, HStack } from "native-base";
 
 export default function SettingsPage() {
   return (
     <Center>
-      <HStack pl={"12px"} pr={"12px"}>
+      <HStack pl={"12px"} pr={"12px"} pb={"12px"}>
         <ArrowButton label="Dashboard" />
       </HStack>
-      <HStack pl={"12px"} pr={"12px"}>
+      <HStack pl={"12px"} pr={"12px"} pb={"12px"}>
         <ArrowButton label="Definições da Conta" />
       </HStack>
-      <HStack pl={"12px"} pr={"12px"}>
+      <HStack pl={"12px"} pr={"12px"} pb={"12px"}>
         <ArrowButton label="Pedidos" />
       </HStack>
-      <HStack pl={"12px"} pr={"12px"}>
-        <ArrowButton label="Dashboard" />
+      <HStack pl={"12px"} pr={"12px"} pb={"12px"}>
+        <ArrowButton label="Notificações" />
       </HStack>
-      <HStack pl={"12px"} pr={"12px"}>
-        <ArrowButton label="Dashboard" />
+      <HStack pl={"12px"} pr={"12px"} pb={"12px"}>
+        <ArrowButton label="Terminar Sessão" />
       </HStack>
+
+      <NavBar />
     </Center>
   );
 }
