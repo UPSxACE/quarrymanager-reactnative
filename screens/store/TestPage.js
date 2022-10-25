cca1055d4c09880d4109fa450326921d6114b295
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-10-25 01:06:00 +0100

    packages #3

--- screens/store/TestPage.js
+++ screens/store/TestPage.js
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
-import SearchBar from '../../components/store/SearchBar';
+import { StyleSheet, Text, View } from "react-native";
+
+import SearchBar from "../../components/store/SearchBar";
 
 export default function TestPage({ navigation }) {
   return (
     <View style={page_styles.container}>
       <Text
         style={page_styles.text1}
-        onPress={() => navigation.navigate('About')}
+        onPress={() => navigation.navigate("About")}
       >
-        Teste
+        Testkk
       </Text>
     </View>
   );
@@ -17,10 +18,10 @@ export default function TestPage({ navigation }) {
 const page_styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 
   text1: {
-    color: 'grey',
+    color: "grey",
   },
 });
