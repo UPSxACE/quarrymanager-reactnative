9ce5f75c1e3f092b96bf569c460ffe5579db3b96
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-10-25 10:33:41 +0100

    conflito e bugfix #3

--- components/store/WhiteButton.js
+++ components/store/WhiteButton.js
@@ -2,20 +2,20 @@ import { Text, HStack, Button } from 'native-base';
 
 export default function WhiteButton({ label }) {
   return (
-    <HStack paddingLeft="12px" paddingRight="12px">
+    <HStack paddingLeft='12px' paddingRight='12px'>
       <Button
         _pressed={{ bg: 'gray.300' }}
-        bg="main.greyComponentBg"
+        bg='main.greyComponentBg'
         width={'100%'}
         height={'40px'}
-        rounded="0"
-        borderBottomWidth="1"
-        borderBottomColor="main.B"
+        rounded='0'
+        borderBottomWidth='1'
+        borderBottomColor='main.B'
       >
         <Text
-          fontSize="14px"
+          fontSize='14px'
           color={'main.greyComponentText'}
-          fontWeight="bold"
+          fontWeight='bold'
         >
           {label}
         </Text>
--- components/store/WhiteInput.js
+++ components/store/WhiteInput.js
@@ -6,25 +6,25 @@ export default function WhiteInput({ label, secret, ...props }) {
   const [show, setShow] = React.useState(!secret);
   return (
     <Input
-      height="40px"
+      height='40px'
       width={'100%'}
       placeholder={label}
       type={show ? 'text' : 'password'}
       InputRightElement={
         secret && (
           <Pressable onPress={() => setShow(!show)}>
-            <Icon as={<Feather name={show ? 'eye' : 'eye-off'} />} mr="2" />
+            <Icon as={<Feather name={show ? 'eye' : 'eye-off'} />} mr='2' />
           </Pressable>
         )
       }
-      rounded="0"
-      bg="main.greyComponentBg"
-      borderWidth="0"
-      borderBottomWidth="1"
-      borderBottomColor="main.B"
+      rounded='0'
+      bg='main.greyComponentBg'
+      borderWidth='0'
+      borderBottomWidth='1'
+      borderBottomColor='main.B'
       _focus={{ bg: 'gray.200' }}
-      placeholderTextColor="#6C6B6B"
-      color="greyComponentText"
+      placeholderTextColor='#6C6B6B'
+      color='greyComponentText'
       {...props}
     />
   );
