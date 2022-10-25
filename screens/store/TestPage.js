803605a8a36890bc55b1e60a971715bc63ea95b8
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-10-25 01:11:51 +0100

    teste #3

--- screens/store/TestPage.js
+++ screens/store/TestPage.js
@@ -1,4 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Icon, Button } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import SearchBar from "../../components/store/SearchBar";
 
@@ -11,6 +16,7 @@ export default function TestPage({ navigation }) {
       >
         Testkk
       </Text>
+      <Icon size="6" color="#000" as={<Feather name="home" />} />;
     </View>
   );
 }
