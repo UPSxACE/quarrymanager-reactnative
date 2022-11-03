a8b77dbd11fab6f970a51cfe9b4fb45cc0e2e386
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-11-03 00:12:33 +0000

    feature: Comp/ScrollableTabs close #15

---
+++ components/store/ScrollableTabs.js
@@ -0,0 +1,24 @@
+import { Text, FlatList } from 'native-base';
+
+const item = ({ item, index }) => {
+  return (
+    <Text fontWeight={'medium'} fontSize={'16px'} padding={'12px'}>
+      {item}
+    </Text>
+  );
+};
+
+const ScrollableTabs = ({ tabs }) => {
+  return (
+    <FlatList
+      bg={'white'}
+      horizontal={true}
+      data={tabs}
+      renderItem={item}
+      keyExtractor={(item, index) => index.toString()}
+      showsHorizontalScrollIndicator={false}
+    />
+  );
+};
+
+export default ScrollableTabs;
--- screens/store/TestPage2.js
+++ screens/store/TestPage2.js
@@ -1,5 +1,21 @@
-import { View } from 'react-native';
+import { View } from 'native-base';
+import ScrollableTabs from '../../components/store/ScrollableTabs';
+
+const TABS_DATA = [
+  'Mármores',
+  'Granitos',
+  'Esmeraldas',
+  'Diamantes',
+  'Porcelana',
+  'Pedras',
+  'Rochas',
+  'Favoritos',
+];
 
 export default function TestPage2({ navigation }) {
-  return <View></View>;
+  return (
+    <View>
+      <ScrollableTabs tabs={TABS_DATA} />
+    </View>
+  );
 }
