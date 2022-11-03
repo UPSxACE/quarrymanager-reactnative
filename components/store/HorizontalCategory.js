e9bcf8e10ef4ed7110d8343fe6a711cf8ce9fed5
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-11-03 16:31:30 +0000

    flatlist e scroll #20

--- components/store/HorizontalCategory.js
+++ components/store/HorizontalCategory.js
@@ -1,14 +1,51 @@
 import { HStack, Text, Image, VStack, FlatList } from "native-base";
 import { StyleSheet } from "react-native";
 
-
-
-
-export default function HorizontalCategory({ imageUrl }) {
+function Item({ id, title, price, imageUrl }) {
   return (
-    <HStack>
-      <FlatList data={DATA} renderItem=[renderItem] keyExtractor={(item) => item.id}></FlatList>
-    </HStack>
+    <VStack>
+      <HStack justifyContent={"space-evenly"} alignItems={"center"}>
+        <VStack>
+          <Image
+            marginTop={"12px"}
+            alignSelf={"center"}
+            alt="Product Picture"
+            style={page_styles.productPicture}
+            source={{
+              uri: imageUrl,
+            }}
+          />
+          <Text>{title}</Text>
+          <Text>{price}</Text>
+        </VStack>
+      </HStack>
+    </VStack>
+  );
+}
+
+function renderItem({ item }) {
+  return (
+    <Item
+      id={item.id}
+      title={item.title}
+      price={item.price}
+      imageUrl={item.imageUrl}
+    />
+  );
+}
+
+export default function HorizontalCategory({ categoryTitle, data }) {
+  return (
+    <VStack>
+      <Text>{categoryTitle}</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
+    </VStack>
 
     /*
     <HStack>
--- screens/store/TestPage2.js
+++ screens/store/TestPage2.js
@@ -14,6 +14,30 @@ const TABS_DATA = [
   "Favoritos",
 ];
 
+const CATEGORY_DATA = [
+  {
+    id: 1,
+    title: "Mármore Preto",
+    price: "30",
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/9900/09d3/37b98db49fb5af7a77035c2f00f8ff77?Expires=1668384000&Signature=TEtfqQl2oJB-PytQ9XJbOlBW~QKqwvrfLfsXqT2pFyvaJWY~vRbOY7a1t2BHRvy5n8CZkgPPhbtrnfBZPJlx7aGocqYDnhc8yWuvHHDWGesfhi9DyBPKHMt7s7t1ePxTNAgoIQ~-rxEkzf-SZdIChx2ZFDE-DtQ597NQmvyWigN1UGprKKkq45xuBJOeS--q7cC39VVlFyV16ysu3Lw0AB-u9yx0qnWaU0B3gi0v-j9YnbUM21EzV2b45vA5qLKgtuLt8REl9jaRBF7TR36QTbtwPLXxdK~SL6uOpTr7gX426QGWHa9svV772I3u4v9KvdQNPFcEteu9YCmpm5iJuA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  },
+  {
+    id: 2,
+    title: "Granito Branco",
+    price: "40",
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/9900/09d3/37b98db49fb5af7a77035c2f00f8ff77?Expires=1668384000&Signature=TEtfqQl2oJB-PytQ9XJbOlBW~QKqwvrfLfsXqT2pFyvaJWY~vRbOY7a1t2BHRvy5n8CZkgPPhbtrnfBZPJlx7aGocqYDnhc8yWuvHHDWGesfhi9DyBPKHMt7s7t1ePxTNAgoIQ~-rxEkzf-SZdIChx2ZFDE-DtQ597NQmvyWigN1UGprKKkq45xuBJOeS--q7cC39VVlFyV16ysu3Lw0AB-u9yx0qnWaU0B3gi0v-j9YnbUM21EzV2b45vA5qLKgtuLt8REl9jaRBF7TR36QTbtwPLXxdK~SL6uOpTr7gX426QGWHa9svV772I3u4v9KvdQNPFcEteu9YCmpm5iJuA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  },
+  {
+    id: 3,
+    title: "Granito Branco",
+    price: "60",
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/9900/09d3/37b98db49fb5af7a77035c2f00f8ff77?Expires=1668384000&Signature=TEtfqQl2oJB-PytQ9XJbOlBW~QKqwvrfLfsXqT2pFyvaJWY~vRbOY7a1t2BHRvy5n8CZkgPPhbtrnfBZPJlx7aGocqYDnhc8yWuvHHDWGesfhi9DyBPKHMt7s7t1ePxTNAgoIQ~-rxEkzf-SZdIChx2ZFDE-DtQ597NQmvyWigN1UGprKKkq45xuBJOeS--q7cC39VVlFyV16ysu3Lw0AB-u9yx0qnWaU0B3gi0v-j9YnbUM21EzV2b45vA5qLKgtuLt8REl9jaRBF7TR36QTbtwPLXxdK~SL6uOpTr7gX426QGWHa9svV772I3u4v9KvdQNPFcEteu9YCmpm5iJuA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  },
+];
+
 export default function TestPage2({ navigation }) {
   return (
     <View>
@@ -21,7 +45,7 @@ export default function TestPage2({ navigation }) {
 
       <BasicList />
 
-      <HorizontalCategory imageUrl="https://disneyplusbrasil.com.br/wp-content/uploads/2022/02/Johnny-Depp2.jpg" />
+      <HorizontalCategory categoryTitle={"Mármores"} data={CATEGORY_DATA} />
     </View>
   );
 }
