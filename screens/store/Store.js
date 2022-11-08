2e0b2a0f480671450778cf7412447cad039751df
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-11-08 16:04:44 +0000

    componentes e ajustes #24

--- screens/store/Store.js
+++ screens/store/Store.js
@@ -43,6 +43,77 @@ export default function Store() {
     "Favoritos",
   ];
 
+  const CATEGORY_DATA1 = [
+    {
+      id: 1,
+      title: "Mármore Preto",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/9900/09d3/37b98db49fb5af7a77035c2f00f8ff77?Expires=1668384000&Signature=TEtfqQl2oJB-PytQ9XJbOlBW~QKqwvrfLfsXqT2pFyvaJWY~vRbOY7a1t2BHRvy5n8CZkgPPhbtrnfBZPJlx7aGocqYDnhc8yWuvHHDWGesfhi9DyBPKHMt7s7t1ePxTNAgoIQ~-rxEkzf-SZdIChx2ZFDE-DtQ597NQmvyWigN1UGprKKkq45xuBJOeS--q7cC39VVlFyV16ysu3Lw0AB-u9yx0qnWaU0B3gi0v-j9YnbUM21EzV2b45vA5qLKgtuLt8REl9jaRBF7TR36QTbtwPLXxdK~SL6uOpTr7gX426QGWHa9svV772I3u4v9KvdQNPFcEteu9YCmpm5iJuA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+    {
+      id: 2,
+      title: "Mármore Branco",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/6fe9/f92b/8d26c16d86c04d3355094fc9852fe7d0?Expires=1668384000&Signature=dBpxVaRGdTi~2uqDQeVIbMwaeUXt1V1OTojBI~mmaQjao3yEBSibjNpgfkJ-g6yqEb2C7NPnxZx7v-rKUKE41sa-9Et9s~WBBtQ6MR8ec8Qmm3JBfPp3L7raBczl-~vhs3U6GFOtGdDCjhbqFX7SIzumkz2ivv0sJ6V3SIbUVg4yQ8p1impTuiShTjuvvAuPq-cW4lwhnLsQvEiABw9QLHcPEJD8Qyn0vjQC78ZAmi0UwKd5hwco-6OuJL~vc6efLJreuJR-FMDBIUaA7zCo2n296iPARRHRNjeLMHf13S-zqeBdGKPRdqoT4ZFNAyhyKW3LgpAcAsYY~KNmw74JJA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+    {
+      id: 3,
+      title: "Mármore Cinza",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/8215/ce33/f54de28244188005d834d681c08c6772?Expires=1668384000&Signature=aHRO76fXy0Np5Zok287ZqwDGk6-7Iv-2nu1A~yThdjtEARkz6t8hTLHI6cxThEQEIr494ns5fAucd5C3i80ywLmxiv4Q~gDRm5U7-g5E1s-9jPVDB7iPppXqdZp-P8E9JiM47H3e~ni23mS-FRgSiAITgAMgtl1I8n95Ija8IBSbsM6Dabiuznw94kfPBjycIcO-8S8HPFOXJkxnqUYL4DzJjp-2XSK2ITzFvcFBxTzOkRFriG-Dfabn5HLpshAAqOyIUGq8sDdCcADKokfyKGlVxR0Aj~6VS8A7qG5lqLU5G4d8b8ATKzVThqKGgamo1dNrI4ogJoyvSmN7SDDW6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+  ];
+
+  const CATEGORY_DATA2 = [
+    {
+      id: 1,
+      title: "Mármore Preto",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/8215/ce33/f54de28244188005d834d681c08c6772?Expires=1668988800&Signature=AeTHXh6z~vxrzXSQalXejP37i6B20~QAiYNY2p-kl25VSGQF~iQ8I3boh7Q~QAFqNB~LHHn6nw1Y7Dc6AzzzHZAwKJm8UhzhUvmM0GaN0YocetQM2iU6pWjc9xhpJccuT9sqsNrr6VGlqI2EtsEhTMBJZOfSMcdzMu3F140o~U3pu6ANBJoMV3B0JBL0ku97564H0KLV8qrUAnaLT93-mIoQnI8ZK0QeKphMFMp-b93RtmD9B25vTLj4zU9xmvrvPA1DZuyMtSuItF-SZb6btdMAtf5gwYoorcf-slj2tOcB~uVuZOR9MnN7Y6QZS3bkQNXxPJO0pjK19udzmYcRbw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+    {
+      id: 2,
+      title: "Mármore Branco",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/73be/1273/b31a52dd8b94ce8f8ef898bf8ca8f719?Expires=1668988800&Signature=TNKV2nRuXIhpDd9KwlGCVaC0ycC6bi6E-Y6X4u5T7r2ErguwcZ~iIhKFOsRBrIc~Z8R8XhiCnUpN6KIxsatr3vH6zmU8S2yV5R8QelIdHN3fyqfcqyvlA16xSsWm5hc2ZgaGDUk9hg~CjfIwCxV16diOINSEBC9BRh8sjHHBiaOVyAUj-VqhjtC9po-1axeonmmTg5GRHru-jd-1epRsDfDsSGRoST8qFqFqfGhnPShKL~LuCbud-vmbhEJvW4H10KVmFQh-L1gu9AOixuuwxm0xL~IttraPzZCpL7GLp~TOZtnTs18Uh1BXLswuskPDFjJd0L99abgdt1o53jX8CQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+    {
+      id: 3,
+      title: "Mármore Cinza",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/8215/ce33/f54de28244188005d834d681c08c6772?Expires=1668384000&Signature=aHRO76fXy0Np5Zok287ZqwDGk6-7Iv-2nu1A~yThdjtEARkz6t8hTLHI6cxThEQEIr494ns5fAucd5C3i80ywLmxiv4Q~gDRm5U7-g5E1s-9jPVDB7iPppXqdZp-P8E9JiM47H3e~ni23mS-FRgSiAITgAMgtl1I8n95Ija8IBSbsM6Dabiuznw94kfPBjycIcO-8S8HPFOXJkxnqUYL4DzJjp-2XSK2ITzFvcFBxTzOkRFriG-Dfabn5HLpshAAqOyIUGq8sDdCcADKokfyKGlVxR0Aj~6VS8A7qG5lqLU5G4d8b8ATKzVThqKGgamo1dNrI4ogJoyvSmN7SDDW6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+  ];
+
+  const CATEGORY_DATA3 = [
+    {
+      id: 1,
+      title: "Mármore Preto",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/9900/09d3/37b98db49fb5af7a77035c2f00f8ff77?Expires=1668384000&Signature=TEtfqQl2oJB-PytQ9XJbOlBW~QKqwvrfLfsXqT2pFyvaJWY~vRbOY7a1t2BHRvy5n8CZkgPPhbtrnfBZPJlx7aGocqYDnhc8yWuvHHDWGesfhi9DyBPKHMt7s7t1ePxTNAgoIQ~-rxEkzf-SZdIChx2ZFDE-DtQ597NQmvyWigN1UGprKKkq45xuBJOeS--q7cC39VVlFyV16ysu3Lw0AB-u9yx0qnWaU0B3gi0v-j9YnbUM21EzV2b45vA5qLKgtuLt8REl9jaRBF7TR36QTbtwPLXxdK~SL6uOpTr7gX426QGWHa9svV772I3u4v9KvdQNPFcEteu9YCmpm5iJuA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+    {
+      id: 2,
+      title: "Mármore Branco",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/6fe9/f92b/8d26c16d86c04d3355094fc9852fe7d0?Expires=1668384000&Signature=dBpxVaRGdTi~2uqDQeVIbMwaeUXt1V1OTojBI~mmaQjao3yEBSibjNpgfkJ-g6yqEb2C7NPnxZx7v-rKUKE41sa-9Et9s~WBBtQ6MR8ec8Qmm3JBfPp3L7raBczl-~vhs3U6GFOtGdDCjhbqFX7SIzumkz2ivv0sJ6V3SIbUVg4yQ8p1impTuiShTjuvvAuPq-cW4lwhnLsQvEiABw9QLHcPEJD8Qyn0vjQC78ZAmi0UwKd5hwco-6OuJL~vc6efLJreuJR-FMDBIUaA7zCo2n296iPARRHRNjeLMHf13S-zqeBdGKPRdqoT4ZFNAyhyKW3LgpAcAsYY~KNmw74JJA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+    {
+      id: 3,
+      title: "Mármore Cinza",
+      price: "10,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/8215/ce33/f54de28244188005d834d681c08c6772?Expires=1668384000&Signature=aHRO76fXy0Np5Zok287ZqwDGk6-7Iv-2nu1A~yThdjtEARkz6t8hTLHI6cxThEQEIr494ns5fAucd5C3i80ywLmxiv4Q~gDRm5U7-g5E1s-9jPVDB7iPppXqdZp-P8E9JiM47H3e~ni23mS-FRgSiAITgAMgtl1I8n95Ija8IBSbsM6Dabiuznw94kfPBjycIcO-8S8HPFOXJkxnqUYL4DzJjp-2XSK2ITzFvcFBxTzOkRFriG-Dfabn5HLpshAAqOyIUGq8sDdCcADKokfyKGlVxR0Aj~6VS8A7qG5lqLU5G4d8b8ATKzVThqKGgamo1dNrI4ogJoyvSmN7SDDW6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+  ];
   return (
     <View>
       <ScrollableTabs tabs={TABS_DATA} />
@@ -58,7 +129,9 @@ export default function Store() {
         </View>
       </PagerView>
 
-      <HorizontalCategory />
+      <HorizontalCategory categoryTitle={"Mármores"} data={CATEGORY_DATA1} />
+      <HorizontalCategory categoryTitle={"Granitos"} data={CATEGORY_DATA2} />
+      <HorizontalCategory categoryTitle={"Esmeraldas"} data={CATEGORY_DATA3} />
     </View>
   );
   return (
