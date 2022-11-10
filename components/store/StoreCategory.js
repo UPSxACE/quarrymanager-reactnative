de6af41245456c6b9fcc138e72d43cec372f0cc4
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-11-10 11:31:56 +0000

    feature: Comp/StoreCategory close #28

--- components/store/StoreCategory.js
+++ components/store/StoreCategory.js
@@ -1,12 +1,26 @@
 import { HStack, Text, Image, VStack, FlatList, View } from "native-base";
 import { StyleSheet } from "react-native";
 
-function Item({ title, price, imageUrl, description }) {
+function Item({ id, title, price, imageUrl, description }) {
   return (
-    <VStack>
-      <VStack alignItems={"center"}>
+    <VStack w={"50%"} alignItems={"center"}>
+      <VStack
+        alignItems={"center"}
+        bg={"red.800"}
+        w={"160px"}
+        h={"auto"}
+        paddingTop={"12px"}
+        paddingBottom={"10px"}
+        style={{
+          elevation: 4,
+          shadowColor: "#000",
+          backgroundColor: "#F8F8F8",
+          marginTop: 6,
+          marginBottom: 6,
+          borderRadius: 4,
+        }}
+      >
         <Image
-          alignSelf={"center"}
           alt="Product Picture"
           style={page_styles.productPicture}
           source={{
@@ -14,19 +28,21 @@ function Item({ title, price, imageUrl, description }) {
           }}
         />
 
-        <View
-          style={{
-            elevation: 4,
-            shadowColor: "#000",
-            backgroundColor: "white",
-            marginTop: 12,
-            borderRadius: 4,
-          }}
-        ></View>
-
-        <Text textAlign={"center"}>{title}</Text>
-        <Text textAlign={"center"}>{description}</Text>
-        <Text textAlign={"center"}>{price}</Text>
+        <Text fontWeight="bold" fontSize={16}>
+          {title}
+        </Text>
+        <Text
+          numberOfLines={2}
+          ellipsizeMode={"tail"}
+          fontSize={10}
+          paddingLeft={"5px"}
+          paddingRight={"5px"}
+        >
+          {description}
+        </Text>
+        <Text fontWeight="bold" fontSize={18}>
+          {price}
+        </Text>
       </VStack>
     </VStack>
   );
@@ -44,16 +60,28 @@ function renderItem({ item }) {
   );
 }
 
-export default function StoreCategory({ categoryResults, CATEGORY_DATA }) {
+export default function StoreCategory({ categoryResults, data }) {
   return (
-    <VStack paddingLeft={"12px"} paddingTop={"6px"} paddingBottom={"12px"}>
-      <Text fontWeight="bold" fontSize={"18px"}>
-        {categoryResults}
-      </Text>
+    <VStack
+      paddingLeft={"12px"}
+      paddingRight={"12px"}
+      paddingTop={"6px"}
+      paddingBottom={"12px"}
+    >
+      <HStack>
+        <Text fontWeight="bold" fontSize={"18px"}>
+          {categoryResults.toUpperCase()}
+        </Text>
+        <Text fontSize={"16px"} marginLeft={"auto"} color={"gray.400"}>
+          8-10
+        </Text>
+      </HStack>
       <FlatList
-        data={CATEGORY_DATA}
+        data={data}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
+        numColumns={2}
+        horizontal={false}
       />
     </VStack>
   );
--- screens/store/Orders.js
+++ screens/store/Orders.js
@@ -9,7 +9,17 @@ export default function Orders(props) {
     {
       id: 1,
       title: "Mármore Preto",
-      description: "Lorem Ipsum",
+      description:
+        "When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, ",
+      price: "202,99€/m²",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/9900/09d3/37b98db49fb5af7a77035c2f00f8ff77?Expires=1668384000&Signature=TEtfqQl2oJB-PytQ9XJbOlBW~QKqwvrfLfsXqT2pFyvaJWY~vRbOY7a1t2BHRvy5n8CZkgPPhbtrnfBZPJlx7aGocqYDnhc8yWuvHHDWGesfhi9DyBPKHMt7s7t1ePxTNAgoIQ~-rxEkzf-SZdIChx2ZFDE-DtQ597NQmvyWigN1UGprKKkq45xuBJOeS--q7cC39VVlFyV16ysu3Lw0AB-u9yx0qnWaU0B3gi0v-j9YnbUM21EzV2b45vA5qLKgtuLt8REl9jaRBF7TR36QTbtwPLXxdK~SL6uOpTr7gX426QGWHa9svV772I3u4v9KvdQNPFcEteu9YCmpm5iJuA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    },
+    {
+      id: 2,
+      title: "Mármore Preto",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500",
       price: "202,99€/m²",
       imageUrl:
         "https://s3-alpha-sig.figma.com/img/9900/09d3/37b98db49fb5af7a77035c2f00f8ff77?Expires=1668384000&Signature=TEtfqQl2oJB-PytQ9XJbOlBW~QKqwvrfLfsXqT2pFyvaJWY~vRbOY7a1t2BHRvy5n8CZkgPPhbtrnfBZPJlx7aGocqYDnhc8yWuvHHDWGesfhi9DyBPKHMt7s7t1ePxTNAgoIQ~-rxEkzf-SZdIChx2ZFDE-DtQ597NQmvyWigN1UGprKKkq45xuBJOeS--q7cC39VVlFyV16ysu3Lw0AB-u9yx0qnWaU0B3gi0v-j9YnbUM21EzV2b45vA5qLKgtuLt8REl9jaRBF7TR36QTbtwPLXxdK~SL6uOpTr7gX426QGWHa9svV772I3u4v9KvdQNPFcEteu9YCmpm5iJuA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
@@ -19,7 +29,7 @@ export default function Orders(props) {
   return (
     <View>
       <ScrollableTabs tabs={TABS_DATA} />
-      <StoreCategory tabs={CATEGORY_DATA} categoryResults={"RESULTADOS"} />
+      <StoreCategory data={CATEGORY_DATA} categoryResults={"Resultados"} />
       <BasicList />
     </View>
   );
