2c4fb1fd5198666066ee36bbe2c3c8113831cc29
Diogo Carvalho <a44765@alunos.ipb.pt>
2022-11-14 22:44:44 +0000

    pager View#31

--- screens/dashboard/DashboardLotes.js
+++ screens/dashboard/DashboardLotes.js
@@ -1,20 +1,29 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 import PagerView from "react-native-pager-view";
 
 export default function DashboardLotes() {
+  const data = [
+    {
+      text: "aso",
+    },
+    {
+      text: "123",
+    },
+    {
+      text: "a1s2d4",
+    },
+  ];
+
   return (
     <View style={{ flex: 1 }}>
       <PagerView style={styles.viewPager} initialPage={0}>
-        <View style={styles.page} key="1">
-          <Text>First page</Text>
-          <Text>Swipe ➡️</Text>
-        </View>
-        <View style={styles.page} key="2">
-          <Text>Second page</Text>
-        </View>
-        <View style={styles.page} key="3">
-          <Text>Third page</Text>
-        </View>
+        {data.map((obj, index) => {
+          return (
+            <View style={styles.page} key={index + 1}>
+              <Text>{obj.text}</Text>
+            </View>
+          );
+        })}
       </PagerView>
     </View>
   );
