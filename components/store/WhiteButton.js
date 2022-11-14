956edc75062e1c3b4b6da3a6ce0c7814b8b63cc8
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-11-14 14:34:58 +0000

    reescrever componentes part 6, StyleOnPressed.Button

--- components/store/WhiteButton.js
+++ components/store/WhiteButton.js
@@ -1,25 +1,54 @@
-import { Text, HStack, Button } from 'native-base';
+import { Text, View } from 'react-native';
+import { themeColors } from '../../Theme';
+import StyledOnPress from '../StyledOnPressed';
 
 export default function WhiteButton({ label }) {
   return (
-    <HStack paddingLeft='12px' paddingRight='12px'>
-      <Button
-        _pressed={{ bg: 'gray.300' }}
-        bg='main.greyComponentBg'
-        width={'100%'}
-        height={'40px'}
-        rounded='0'
-        borderBottomWidth='1'
-        borderBottomColor='main.B'
-      >
-        <Text
-          fontSize='14px'
-          color={'main.greyComponentText'}
-          fontWeight='bold'
+    <>
+      <View style={{ paddingHorizontal: 12 }}>
+        <StyledOnPress.Button
+          style={{
+            backgroundColor: themeColors.main.greyComponentBg,
+            height: 40,
+            borderBottomWidth: 1,
+            borderBottomColor: themeColors.main.B,
+          }}
+          //onPressStyle={{ height: 80 }}
+          onPressColor={'#d4d4d8'}
         >
-          {label}
-        </Text>
-      </Button>
-    </HStack>
+          <Text
+            style={{
+              fontSize: 14,
+              color: themeColors.main.greyComponentText,
+              fontWeight: 'bold',
+            }}
+          >
+            Submit
+          </Text>
+        </StyledOnPress.Button>
+      </View>
+    </>
   );
 }
+
+/*
+<HStack paddingLeft="12px" paddingRight="12px">
+        <Button
+          _pressed={{ bg: 'gray.300' }}
+          bg="main.greyComponentBg"
+          width={'100%'}
+          height={'40px'}
+          rounded="0"
+          borderBottomWidth="1"
+          borderBottomColor="main.B"
+        >
+          <Text
+            fontSize="14px"
+            color={'main.greyComponentText'}
+            fontWeight="bold"
+          >
+            {label}
+          </Text>
+        </Button>
+      </HStack>
+      */
