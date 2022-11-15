6a402a583b9fd21760d41db9ec8225ede566c536
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-11-15 11:11:09 +0000

    picture e styles

--- screens/store/OrderProduct.js
+++ screens/store/OrderProduct.js
@@ -1,8 +1,22 @@
-import { View, StyleSheet, FlatList, Text } from "react-native";
+import { View, StyleSheet, FlatList, Text, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-export default function OrderProduct({ navigation }) {
-  return <View height="100%" backgroundColor="black"></View>;
+export default function OrderProduct({ navigation, title, reference }) {
+  return (
+    <View>
+      <Image
+        alt="Order Pic"
+        style={page_styles.orderPicture}
+        source={{
+          uri: "https://media-manager.noticiasaominuto.com/1920/naom_5fa664745ba42.jpg",
+        }}
+      />
+      <View style={{ alignItems: "center", flex: 1 }}>
+        <Text style={page_styles.titleText}>{title}</Text>
+        <Text style={page_styles.processText}>{reference}</Text>
+      </View>
+    </View>
+  );
 }
 
 const page_styles = StyleSheet.create({
@@ -11,6 +25,28 @@ const page_styles = StyleSheet.create({
     alignItems: "center",
     marginLeft: 12,
     marginRight: 12,
-    backgroundColor: "black",
+    backgroundColor: "#f5f5f5",
+  },
+
+  orderPicture: {
+    width: 130,
+    height: 100,
+    borderRadius: 4,
+    marginHorizontal: 12,
+    marginTop: 12,
+    marginBottom: 12,
+    alignSelf: "center",
+  },
+
+  titleText: {
+    color: "black",
+    fontWeight: "bold",
+    lineHeight: 16,
+  },
+
+  processText: {
+    color: "#6E7173",
+    fontStyle: "italic",
+    lineHeight: 16,
   },
 });
