36558af0d344b4aa5a48ae5e9602e6a8f4349faa
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-11-15 11:56:58 +0000

    styles #27

--- screens/store/OrderProduct.js
+++ screens/store/OrderProduct.js
@@ -1,19 +1,157 @@
-import { View, StyleSheet, FlatList, Text, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  Image,
+  Pressable,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import WhiteInput from "../../components/store/WhiteInput";
 
 export default function OrderProduct({ navigation, title, reference }) {
   return (
-    <View>
-      <Image
-        alt="Order Pic"
-        style={page_styles.orderPicture}
-        source={{
-          uri: "https://media-manager.noticiasaominuto.com/1920/naom_5fa664745ba42.jpg",
+    <View style={page_styles.container}>
+      <View
+        style={{
+          alignItems: "center",
+          flexDirection: "row",
+          backgroundColor: "white",
+          width: "100%",
+          marginBottom: 12,
         }}
-      />
-      <View style={{ alignItems: "center", flex: 1 }}>
-        <Text style={page_styles.titleText}>{title}</Text>
-        <Text style={page_styles.processText}>{reference}</Text>
+      >
+        <Image
+          alt="Order Pic"
+          style={page_styles.orderPicture}
+          source={{
+            uri: "https://media-manager.noticiasaominuto.com/1920/naom_5fa664745ba42.jpg",
+          }}
+        />
+        <View style={{ alignItems: "center", flex: 1 }}>
+          <Text style={page_styles.titleText}>Mármore Preto</Text>
+          <Text style={page_styles.processText}>Ref: 00023456</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "white",
+          width: "93%",
+          marginBottom: 2,
+          marginLeft: 12,
+          paddingLeft: 12,
+          borderRadius: 4,
+        }}
+      >
+        <View>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+          >
+            Pedido
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            paddingRight: 12,
+          }}
+        >
+          <Text>Quantidade(m²):</Text>
+          <Text
+            style={{
+              marginLeft: "auto",
+            }}
+          >
+            Fazer a caixinha
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            paddingRight: 12,
+          }}
+        >
+          <Text>Preço(€/m²):</Text>
+          <Text
+            style={{
+              marginLeft: "auto",
+            }}
+          >
+            10,99€
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            paddingRight: 12,
+          }}
+        >
+          <Text>Desconto:</Text>
+          <Text
+            style={{
+              marginLeft: "auto",
+            }}
+          >
+            0€
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            paddingRight: 12,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+          >
+            Total:
+          </Text>
+          <Text
+            style={{
+              marginLeft: "auto",
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+          >
+            21,98€
+          </Text>
+        </View>
+
+        <Pressable onPress={() => navigation.navigate("Home")}>
+          <Text
+            style={{
+              fontSize: 18,
+              color: "#0050A7",
+            }}
+          >
+            Adicionar Código de Desconto
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={{ flexDirection: "row", width: "100%", marginTop: 12 }}>
+        <Text>Dados de entrega</Text>
+
+        <Pressable
+          onPress={() => navigation.navigate("Home")}
+          style={{ marginLeft: "auto" }}
+        >
+          <Text>Editar</Text>
+        </Pressable>
+      </View>
+      <View>
+        <View style={{ marginTop: 12 }}>
+          <WhiteInput label={"Primeiro Nome"} />
+        </View>
+        <View style={{ marginTop: 12 }}>
+          <WhiteInput label={"Último Nome"} />
+        </View>
       </View>
     </View>
   );
@@ -22,10 +160,7 @@ export default function OrderProduct({ navigation, title, reference }) {
 const page_styles = StyleSheet.create({
   container: {
     width: "100%",
-    alignItems: "center",
-    marginLeft: 12,
-    marginRight: 12,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#f3f3f3",
   },
 
   orderPicture: {
@@ -44,7 +179,7 @@ const page_styles = StyleSheet.create({
     lineHeight: 16,
   },
 
-  processText: {
+  refText: {
     color: "#6E7173",
     fontStyle: "italic",
     lineHeight: 16,
