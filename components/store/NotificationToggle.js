be1a38e983f33523cdf809145db9750c4c6349d7
Gabriel Afonso Pires <a50400@alunos.ipb.pt>
2022-11-24 09:45:39 +0000

    Inicialização do componente NotificationToggle#35

---
+++ components/store/NotificationToggle.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export default function NotificationToggle() {
+  return (
+    <View style={{ flexDirection: "row-reverse", width: "100%" }}>
+      <View></View>
+      <View>
+        <Text>Ativar Notificações de Telemóvel</Text>
+      </View>
+      <View></View>
+    </View>
+  );
+}
