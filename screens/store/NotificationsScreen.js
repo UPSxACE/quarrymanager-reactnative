c94d2a256d5901b4bd902f8abcdb081460f3e6a4
Diogo Carvalho <a44765@alunos.ipb.pt>
2022-12-06 12:26:58 +0000

    notify page

--- screens/store/NotificationsScreen.js
+++ screens/store/NotificationsScreen.js
@@ -1,10 +1,53 @@
 import NotificationToggle from "../../components/store/NotificationToggle";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function NotificationsScreen() {
   return (
     <View>
-      <NotificationToggle />
+      <View style={styles.viewStyle}>
+        <Text style={styles.title}>Notificações Gerais</Text>
+
+        <View style={{ paddingHorizontal: 8 }}>
+          <NotificationToggle
+            label={"Ativar Notificações de Telemóvel"}
+            iconName={"bell-off"}
+          />
+        </View>
+        <View style={{ paddingHorizontal: 8 }}>
+          <NotificationToggle
+            label={"Som das Notificações"}
+            iconName={"volume-x"}
+          />
+        </View>
+      </View>
+      <View style={styles.viewStyle}>
+        <Text style={styles.title}>Notificações Personalizadas</Text>
+        <View style={{ paddingHorizontal: 8 }}>
+          <NotificationToggle label={"Mensagem"} iconName={"mail"} />
+        </View>
+        <View style={{ paddingHorizontal: 8 }}>
+          <NotificationToggle label={"Encomendas"} iconName={"box"} />
+        </View>
+        <View style={{ paddingHorizontal: 8 }}>
+          <NotificationToggle label={"Promoções"} iconName={"dollar-sign"} />
+        </View>
+        <View style={{ paddingHorizontal: 8 }}>
+          <NotificationToggle label={"Novidades"} iconName={"tag"} />
+        </View>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 32,
+    paddingStart: 8,
+    marginBottom: 16,
+  },
+  viewStyle: {
+    paddingStart: 8,
+    paddingEnd: 8,
+  },
+});
