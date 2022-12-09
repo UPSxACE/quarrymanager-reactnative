d681cd650f3b6c6fde9d51172956e4fa1b843e0f
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-12-09 20:42:45 +0000

    notificacoes

--- components/store/NotificationToggle.js
+++ components/store/NotificationToggle.js
@@ -1,40 +1,46 @@
-import { View, Text } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import { Switch } from "react-native-paper";
-import { themeColors } from "../../Theme";
+import { View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { Switch } from 'react-native-paper';
+import { themeColors } from '../../Theme';
+import { useState } from 'react';
 
 export default function NotificationToggle({
   label,
   iconName,
   notificationName,
 }) {
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+
   return (
-    <View style={{ flexDirection: "row", width: "100%", borderBottomWidth: 1 }}>
+    <View style={{ flexDirection: 'row', width: '100%', borderBottomWidth: 1 }}>
       <View
         style={{
           height: 52,
           width: 52,
-          justifyContent: "center",
-          alignItems: "center",
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <Feather size={28} color={"#6C6B6B"} name={iconName}></Feather>
+        <Feather size={28} color={'black'} name={iconName}></Feather>
       </View>
-      <View style={{ textAlign: "center", flex: 1, justifyContent: "center" }}>
+      <View style={{ textAlign: 'center', flex: 1, justifyContent: 'center' }}>
         <Text>{label}</Text>
         <Text style={{ color: themeColors.main.secondaryTextColor }}>
-          Desativado
+          {isSwitchOn ? 'Ativado' : 'Desativado'}
         </Text>
       </View>
       <View
         style={{
           height: 52,
-          justifyContent: "center",
-          alignItems: "center",
+          justifyContent: 'center',
+          alignItems: 'center',
           paddingRight: 12,
         }}
       >
-        <Switch>{notificationName}</Switch>
+        <Switch value={isSwitchOn} onValueChange={onToggleSwitch}>
+          {notificationName}
+        </Switch>
       </View>
     </View>
   );
