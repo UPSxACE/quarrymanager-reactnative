cffa0915cb26d3ef5b21e6ff3abb2c85931ac68c
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2023-01-23 14:57:17 +0000

    firebase

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export default config = {
+  apiKey: 'POR_CONFIGURAR',
+  authDomain: 'POR_CONFIGURAR',
+  databaseURL: 'POR_CONFIGURAR',
+  projectId: 'POR_CONFIGURAR',
+  storageBucket: 'POR_CONFIGURAR',
+  messagingSenderId: 'POR_CONFIGURAR',
+  appId: 'POR_CONFIGURAR',
+};
---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase';
+import 'firebase/database';
+import config from './firebase-config';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+firebase.initializeApp(config);
+export const firebase_db = firebase;
