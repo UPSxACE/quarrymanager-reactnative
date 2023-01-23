d83782c035667dc37ba957a28359c04810135012
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2023-01-23 15:05:26 +0000

    deletar firebase-config do repositorio

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export default config = {
-  apiKey: 'POR_CONFIGURAR',
-  authDomain: 'POR_CONFIGURAR',
-  databaseURL: 'POR_CONFIGURAR',
-  projectId: 'POR_CONFIGURAR',
-  storageBucket: 'POR_CONFIGURAR',
-  messagingSenderId: 'POR_CONFIGURAR',
-  appId: 'POR_CONFIGURAR',
-};
