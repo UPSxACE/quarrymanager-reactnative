6824f7916d958b992a2452a4c3969b43a48ce844
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2023-01-25 16:00:09 +0000

    Criar URL

--- api.js
+++ api.js
@@ -1,7 +1,12 @@
-import apiconfig from './api-config';
+import apiconfig from "./api-config";
+
+const criarUrl = (endpoint) => {
+  return "http://" + apiconfig.serverIP + "/api/" + endpoint;
+};
 
 const api = {
-  listar_produto: 'http://' + apiconfig.serverIP + '/api/produto/listar',
+  listar_produto: criarUrl("produto/listar"),
+  listar_material: criarUrl("material/listar"),
 };
 
 export default api;
