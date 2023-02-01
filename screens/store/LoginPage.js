aba6b89cc4dfe5ccf1f1ade92733f4fa794c8117
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2023-02-01 18:32:38 +0000

    modal login #54

--- screens/store/LoginPage.js
+++ screens/store/LoginPage.js
@@ -5,121 +5,182 @@ import {
   StyleSheet,
   ScrollView,
   Pressable,
+  ActivityIndicator,
 } from 'react-native';
 import BlueInput from '../../components/store/BlueInput';
 import BlueButton from '../../components/store/BlueButton';
-import { Feather } from '@expo/vector-icons';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
 import { themeColors } from '../../Theme';
 import React from 'react';
 import { CommonActions, useNavigation } from '@react-navigation/native';
 import HomeFooter from '../../components/store/HomeFooter';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Modal, Portal, Provider } from 'react-native-paper';
+import axios from 'axios';
+import api from '../../api';
 
 export default function LoginPage({ setLogin }) {
   const [formData, setData] = React.useState({});
   const navigation = useNavigation();
+  const [visible, setVisible] = React.useState(false);
+  const [dismissable, setDismissable] = React.useState(true);
+  const [loginError, setLoginError] = React.useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => {
+    setDismissable(false);
+    setLoginError(false);
+    setVisible(false);
+  };
+  const containerStyle = {
+    backgroundColor: 'white',
+    padding: 20,
+    width: 250,
+    height: 200,
+  };
+
+  const trylogin = () => {
+    const sendRequest = async () => {
+      const result = await axios.post(api.login, { ...formData });
+    };
+
+    sendRequest()
+      .then(() => {
+        console.log('sucesso');
+      })
+      .catch((reason) => {
+        setLoginError(reason.response.data[0].message);
+        setDismissable(true);
+      });
+  };
+
   const resetActionLogin = CommonActions.reset({
     index: 0,
     routes: [{ name: 'HomeStack', params: { screen: 'Home' } }],
   });
 
   return (
-    <View style={{ flex: 1 }}>
-      <ScrollView
-        contentContainerStyle={{
-          justifyContent: 'center',
-          flex: 1,
-        }}
-      >
-        <View style={loginStyles.logo}>
-          <Text style={loginStyles.logoText}>weROCK</Text>
-        </View>
+    <Provider>
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          dismissable={dismissable}
+          contentContainerStyle={containerStyle}
+          style={{ justifyContent: 'center', alignItems: 'center' }}
+        >
+          {loginError ? (
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <MaterialIcons size={80} color={'red'} name={'error'} />
+              <Text>{loginError}</Text>
+            </View>
+          ) : (
+            <ActivityIndicator color={themeColors.main.A} size={80} />
+          )}
+        </Modal>
+      </Portal>
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          contentContainerStyle={{
+            justifyContent: 'center',
+            flex: 1,
+          }}
+        >
+          <View style={loginStyles.logo}>
+            <Text style={loginStyles.logoText}>weROCK</Text>
+          </View>
 
-        <View style={loginStyles.inputBox}>
-          <BlueInput
-            containerStyle={{ borderRadius: 4 }}
-            label='Username'
-            onChangeText={(value) => setData({ ...formData, name: value })}
-          />
-        </View>
-        <View style={loginStyles.inputBox}>
-          <BlueInput
-            containerStyle={{ borderRadius: 4 }}
-            label='Password'
-            onChangeText={(value) => setData({ ...formData, name: value })}
-            secret
-          />
-        </View>
+          <View style={loginStyles.inputBox}>
+            <BlueInput
+              containerStyle={{ borderRadius: 4 }}
+              label="Username"
+              onChangeText={(value) => setData({ ...formData, email: value })}
+            />
+          </View>
+          <View style={loginStyles.inputBox}>
+            <BlueInput
+              containerStyle={{ borderRadius: 4 }}
+              label="Password"
+              onChangeText={(value) =>
+                setData({ ...formData, password: value })
+              }
+              secret
+            />
+          </View>
 
-        <View style={loginStyles.btn}>
-          <BlueButton
-            label='Iniciar sessao'
-            style={{ backgroundColor: '#394A58' }}
-            onPressEvent={async () => {
-              // await
-              navigation.dispatch(resetActionLogin);
-              setLogin(true);
-              AsyncStorage.setItem('login', 'true');
+          <View style={loginStyles.btn}>
+            <BlueButton
+              label="Iniciar sessao"
+              style={{ backgroundColor: '#394A58' }}
+              onPressEvent={async () => {
+                showModal();
+                setTimeout(trylogin, 750);
+                //trylogin();
+                return;
+                // await
+                navigation.dispatch(resetActionLogin);
+                setLogin(true);
+                AsyncStorage.setItem('login', 'true');
+              }}
+            />
+          </View>
+          <Text
+            style={loginStyles.link}
+            onPress={() => {
+              // Linking.openURL('https://twitter.com/?lang=pt');
             }}
-          />
-        </View>
-        <Text
-          style={loginStyles.link}
-          onPress={() => {
-            // Linking.openURL('https://twitter.com/?lang=pt');
-          }}
-        >
-          Esqueceste-te da palavra passe?
-        </Text>
+          >
+            Esqueceste-te da palavra passe?
+          </Text>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            width: '100%',
-            paddingHorizontal: 20,
-            paddingBottom: 40,
-            paddingTop: 20,
-          }}
-        >
           <View
             style={{
-              flex: 1,
-              height: 1,
-              backgroundColor: 'black',
+              flexDirection: 'row',
+              alignItems: 'center',
+              width: '100%',
+              paddingHorizontal: 20,
+              paddingBottom: 40,
+              paddingTop: 20,
             }}
-          />
-          <View>
-            <Text style={{ width: 50, textAlign: 'center' }}>OU</Text>
+          >
+            <View
+              style={{
+                flex: 1,
+                height: 1,
+                backgroundColor: 'black',
+              }}
+            />
+            <View>
+              <Text style={{ width: 50, textAlign: 'center' }}>OU</Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                height: 1,
+                backgroundColor: 'black',
+              }}
+            />
           </View>
           <View
             style={{
-              flex: 1,
-              height: 1,
-              backgroundColor: 'black',
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'center',
             }}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            width: '100%',
-            justifyContent: 'center',
-          }}
-        >
-          <Pressable style={loginStyles.iconstyle}>
-            <Feather color='white' size={22} name='facebook'></Feather>
-          </Pressable>
-          <Pressable style={loginStyles.iconstyle}>
-            <Feather color='white' size={22} name='twitter'></Feather>
-          </Pressable>
-          <Pressable style={loginStyles.iconstyle}>
-            <Feather color='white' size={22} name='mail'></Feather>
-          </Pressable>
-        </View>
-      </ScrollView>
-      <HomeFooter />
-    </View>
+          >
+            <Pressable style={loginStyles.iconstyle}>
+              <Feather color="white" size={22} name="facebook"></Feather>
+            </Pressable>
+            <Pressable style={loginStyles.iconstyle}>
+              <Feather color="white" size={22} name="twitter"></Feather>
+            </Pressable>
+            <Pressable style={loginStyles.iconstyle}>
+              <Feather color="white" size={22} name="mail"></Feather>
+            </Pressable>
+          </View>
+        </ScrollView>
+        <HomeFooter />
+      </View>
+    </Provider>
   );
 }
 
